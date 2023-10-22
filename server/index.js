91b2de94596fb53153ece9b24ae85f238eb5dfa3
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2023-10-22 13:21:15 +0200

    Added the structure of the express server for the project

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+dotenv.config();
+const app = express();
+
+
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => console.log(`listening on port: ${PORT} and Database is Connected`)))
+.catch((error) => console.log(error.message));
---
+++ server/routes/posts.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router();
+import { getPosts, createPost } from "../controllers/posts.js";
+
+router.get('/', getPosts);
+router.post('/', createPost);
+
+
+export default router;
