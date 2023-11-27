8f3433f9dedf58346564386d0818cd4b34ecfc5e
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2023-11-27 21:16:19 +0100

    Adding routes for seperate components with React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,29 @@
-import React, { useState, useEffect } from "react";
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from "react-redux";
+import React from "react";
+import { Container } from '@material-ui/core';
+import { ToastProvider } from "./components/Providers/ToastProvider.js";
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
-import Posts from "./components/Posts/Posts.js";
-import Form from "./components/Form/Form.js";
-import { getPosts } from "./actions/posts.js";
-// import Posts from "./components/Posts/Posts.js";
 
-import swedenspectra from "./images/spectra.png";
-import logoGif from "./images/LogoGif.gif";
-import useStyles from "./styles.js"
-
+import Home from "./components/Home/Home.js";
+import Navbar from "./components/Navbar/Navbar.js";
+import Auth from "./components/Auth/Auth.js";
 const App = () => {
-    const classes = useStyles();
-    const dispatch = useDispatch();
-    const [currentId, setCurrentId] = useState(null);
+   
+   return (
 
-    useEffect(() => {   
-        dispatch(getPosts());
-    }, [dispatch]);
+    
+<BrowserRouter> 
+   <Container maxwidth="lg">
+      <ToastProvider>
+        <Navbar/>
+        <Routes>
+            <Route path="/" exact element={<Home/>} />
+            <Route path="/auth" exact element={<Auth/>} />
 
-    return (
-
-       <Container maxwidth="lg">
-
-        <AppBar className={classes.appBar} position="static" color="inherit"> 
-
-        <Typography className={classes.heading} variant="h2" align="center">
-        <img className={classes.image} src={logoGif} alt="memories" height="60"/>
-
-        {/* <span style={{ fontFamily: 'Lobster', color: 'rgb(0, 106, 167)' }}>Sweden</span>
-        <span style={{ fontFamily: 'Lobster', color: 'rgb(254, 204, 2)' }}>Spectra</span> */}
-  {/* <video autoPlay loop muted height="80" width="100">
-        <source src="./images/Logo.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video> */}
-       {/* <video width="80" height="200" controls >
-      <source src="./images/Logo.mp4" type="video/mp4"/>
-     </video> */}
-
-        </Typography>
-
-        </AppBar>
-
-        <Grow in>
-            <Container>
-                <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
-                    <Grid item xs={12} sm={7}>
-                     <Posts setCurrentId={setCurrentId}/>
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                    <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                    </Grid>
-
-                </Grid>
-            </Container>
-
-        </Grow>
-
-       </Container>
+        </Routes>
+      </ToastProvider>
+   </Container>
+</BrowserRouter>
     );
 }
-
 export default App;
