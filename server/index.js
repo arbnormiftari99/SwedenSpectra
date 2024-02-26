d0cf8fdcc200c57ac626cfffb0bb00fac3301369
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-02-26 21:16:49 +0100

    added the right domains for cors

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin:['http://localhost:3000/','https://sweden-spectra.vercel.app/']
+}));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
