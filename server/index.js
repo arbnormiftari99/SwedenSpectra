b79f781400aa34b43a512a369ce5598eeafe943d
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-02-26 21:23:19 +0100

    pt2

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-  origin:['http://localhost:3000/','https://sweden-spectra.vercel.app/']
+  origin:['http://localhost:3000/*','https://sweden-spectra.vercel.app/*']
 }));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
