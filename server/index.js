148a27d3f01732752ab50d49f35ad31e2eca7420
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-03-13 11:18:38 +0100

    no message

--- server/index.js
+++ server/index.js
@@ -15,11 +15,11 @@ app.use(express.json());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-  origin:['https://sweden-spectra.vercel.app'],
+  origin:['https://sweden-spectra.vercel.app/*'],
   methods:['GET','POST','DELETE','OPTIONS','PUT','HEAD']
 }));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://sweden-spectra.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'https://sweden-spectra.vercel.app/*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
