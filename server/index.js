5b4e58f5095877322aaf44e3977acdd8b1a2d387
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-04-15 22:03:25 +0200

    .

--- server/index.js
+++ server/index.js
@@ -15,26 +15,26 @@ app.use(express.json());
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-const corsOptions = {
-  origin: ['https://sweden-spectra.vercel.app'],
-  methods: "GET,HEAD,PUT,OPTIONS,POST,DELETE",
-  allowedHeaders: [
-    "Access-Control-Allow-Headers",
-    "Origin",
-    "X-Requested-With",
-    "Content-Type",
-    "Accept",
-    "Authorization",
-    "token",
-    "Access-Control-Request-Method",
-    "Access-Control-Request-Headers",
-    "Access-Control-Allow-Credentials",
-  ],
-  credentials: true,
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: ['https://sweden-spectra.vercel.app'],
+//   methods: "GET,HEAD,PUT,OPTIONS,POST,DELETE",
+//   allowedHeaders: [
+//     "Access-Control-Allow-Headers",
+//     "Origin",
+//     "X-Requested-With",
+//     "Content-Type",
+//     "Accept",
+//     "Authorization",
+//     "token",
+//     "Access-Control-Request-Method",
+//     "Access-Control-Request-Headers",
+//     "Access-Control-Allow-Credentials",
+//   ],
+//   credentials: true,
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+// };
+// app.use(cors(corsOptions));
 
 // app.use(cors({
 //   origin: 'https://sweden-spectra.vercel.app',
@@ -42,13 +42,13 @@ app.use(cors(corsOptions));
 //   credentials: true
 // }));
 
-// app.use(cors());
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'https://sweden-spectra.vercel.app');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATH, HEAD, PATCH');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//     next();
-//   });
+app.use(cors());
+app.use((req, res, next) => {
+    res.header.append('Access-Control-Allow-Origin', 'https://sweden-spectra.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATH, HEAD, PATCH');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+  });
 
 
 
