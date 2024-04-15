e2e7dd783f632e10d916069862f5b95624347fd2
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-04-15 22:09:24 +0200

    T

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -90,7 +90,7 @@ export const likePost = (id) => async (dispatch) => {
 export const commentPost = (value, id) => async (dispatch) => {
     try {
       const { data } = await api.comment(value, id);
-      dispatch({type: 'COMMENT', payload: data});
+      dispatch({type: COMMENT, payload: data});
       return data.comments;
     } catch (error) {
       console.log(error);
--- server/index.js
+++ server/index.js
@@ -36,19 +36,19 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 // };
 // app.use(cors(corsOptions));
 
-// app.use(cors({
-//   origin: 'https://sweden-spectra.vercel.app',
-//   methods:['GET','POST','DELETE','OPTIONS','PUT','HEAD', 'PATCH'],
-//   credentials: true
-// }));
+app.use(cors({
+  origin: 'https://sweden-spectra.vercel.app',
+  methods:['GET','POST','DELETE','OPTIONS','PUT','HEAD', 'PATCH'],
+  credentials: true
+}));
 
-app.use(cors());
-app.use((req, res, next) => {
-    res.header.append('Access-Control-Allow-Origin', 'https://sweden-spectra.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATH, HEAD, PATCH');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-  });
+// app.use(cors());
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'https://sweden-spectra.vercel.app');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATH, HEAD, PATCH');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//     next();
+//   });
 
 
 
