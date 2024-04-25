7baf3a77ab164b3d7a2253bcf08ad79e4b4e9e82
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-04-25 11:33:27 +0200

    Trying to solve the issue with fetching posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,20 +5,25 @@ const router = express.Router();
 
 
 
-export const getPosts = async (req,res) => {
-    const { page } = req.query;
-  try {
-    const LIMIT = 8
-    const startIndex = (Number(page) - 1) * LIMIT
-    const total = await PostMessage.countDocuments({})
-    const posts = await PostMessage.find().sort({_id: -1}).limit(LIMIT).skip(startIndex);
-
-    res.status(200).json({data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT)});
-} catch (error) {
-    res.status(404).json({message: error.message});
-  }
-}
-
+export const getPosts = async (req, res) => {
+    let { page } = req.query;
+  
+    if (page === undefined || isNaN(Number(page))) {
+      page = 1;
+    }
+  
+    try {
+      const LIMIT = 9;
+      const startIndex = (Number(page) - 1) * LIMIT;
+      const total = await PostMessage.countDocuments({});
+      const posts = await PostMessage.find().sort({_id: -1}).limit(LIMIT).skip(startIndex);
+  
+      res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  };
+  
 
 export const getPostsSearch = async (req,res) => {
     const { searchQuery, tags} = req.query
