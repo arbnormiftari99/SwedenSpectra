58ecfd17d2dd2eff481249111e69043c4f827f73
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2024-05-05 00:02:27 +0200

    Post detail design and structure in a professional way

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -1,102 +1,83 @@
 import React, { useEffect } from 'react';
-import { Paper, Typography, CircularProgress, Divider, Card, CardMedia } from '@material-ui/core';
-import Grid from '@mui/material/Unstable_Grid2';
+import { Paper, Typography, CircularProgress, Divider, Grid } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
 import { useParams, useNavigate } from 'react-router-dom';
 import { getPost, getPostsSearch } from '../../actions/posts';
 import Comments from './Comments';
-
 import useStyles from './styles';
 
-
-
 const PostDetails = () => {
-   const {post, posts, isLoading } = useSelector((state) => state.posts);
-   const dispatch = useDispatch();
-   const navigate = useNavigate();
-   const classes = useStyles();
-   const { id } = useParams();
+    const { post, posts, isLoading } = useSelector((state) => state.posts);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const classes = useStyles();
+    const { id } = useParams();
 
-   useEffect(() => {
-    dispatch(getPost(id));
-   }, [id])
+    useEffect(() => {
+        dispatch(getPost(id));
+    }, [id]);
 
-   useEffect(() => {
-    if(post) {
-      dispatch(getPostsSearch({search: 'none', tags: post && post.tags && post.tags.join(',')}))
+    useEffect(() => {
+        if (post) {
+            dispatch(getPostsSearch({ search: 'none', tags: post && post.tags && post.tags.join(',') }));
+        }
+    }, [post]);
+
+    if (!post) return null;
+
+    if (isLoading) {
+        return (
+            <Paper elevation={6} className={classes.loadingPaper}>
+                <CircularProgress />
+            </Paper>
+        );
     }
-   }, [post])
 
-   if(!post) return null;
+    const recommendedPosts = posts.filter(({ _id }) => _id !== post._id).slice(0, 4);
 
+    const openPost = (_id) => navigate(`/posts/${_id}`);
 
-   if(isLoading){
-    return <Paper elevation={6} className={classes.loadingPaper}>
-      <CircularProgress/>
-    </Paper>
-   }
+    return (
+        <Paper className={classes.paper} elevation={6}>
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={12} md={6}>
+                    <div className={classes.container}>
+                        <img src={post.selectedFile[0].url} alt={post.title} className={classes.mainImage} onClick={() => openPost(post._id)} />
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={12} md={6}>
+                    <div className={classes.detailsSection}>
+                        <Typography variant="h5" className={classes.mainTitle}>{post.title}</Typography>
+                        <Typography variant="subtitle1" className={classes.mainAuthor}>By {post.name}</Typography>
+                        <Typography variant="subtitle2" className={classes.mainCreatedAt}>{moment(post.createdAt).fromNow()}</Typography>
+                        <Typography variant="body1" className={classes.mainMessage}>{post.message}</Typography>
+                        <Divider className={classes.divider} />
+                        <div className={classes.tagsContainer}>
+                            <Typography variant="subtitle2"><strong>Tags:</strong> {post.tags.map((tag) => `#${tag}`)}</Typography>
+                        </div>
+                        <Divider className={classes.divider} />
+                        <Comments post={post} />
+                    </div>
+                </Grid>
+            </Grid>
 
-   const recommendedPosts = posts.filter(({ _id }) => _id !== post._id).slice(0, 4);
+            <div className={classes.recommendedPosts}>
+                <Typography variant="h6">You might also like:</Typography>
+                <Grid container spacing={2}>
+                    {recommendedPosts.map(({ title, likes, selectedFile, _id }) => (
+                        <Grid item xs={12} sm={6} md={3} key={_id}>
+                            <Paper elevation={3} className={classes.recommendedPost} onClick={() => openPost(_id)}>
+                                <img src={selectedFile[0].url} alt={title} className={classes.recommendedImage} />
+                                <Typography variant="subtitle1" className={classes.recommendedTitle}>{title}</Typography>
+                                <Typography variant="subtitle2">Likes: {likes.length}</Typography>
+                            </Paper>
+                        </Grid>
+                    ))}
+                </Grid>
+            </div>
+        </Paper>
+    );
+};
 
-   const openPost = (_id) => navigate(`/posts/${_id}`)
-
-  return (
-    <Paper style={{ padding: '20px', borderRadius: '15px'}} elevation={6}>
-      <div className={classes.card}>
-
-      <div className={classes.imageSection}>
-      {post.selectedFile && post.selectedFile.map((file, index) => (
-       <img key={index} className={classes.media} src={file.url || 'https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg'} alt={post.title} />
-        ))}
-        </div>
-
-        <div className={classes.section}>
-         <div className="commentsDetails">
-          <Typography variant="h3" component="h2" className={classes.titleContainer}>{post.title}</Typography>
-          <Typography gutterBottom variant="body1" component="p" className={classes.messageContainer}>{post.message}</Typography>
-          <Typography variant="h7" color="textSecondary" gutterBottom>Created by: {post.name}</Typography>
-          <Typography gutterBottom variant="h6" color="textSecondary" component="h2" className={classes.messageContainer}><strong>Tags:</strong> {post.tags.map((tag) => `#${tag}`)}</Typography>
-          <Typography variant="body1">{moment(post.createdAt).fromNow()}</Typography>
-          <Divider style={{ margin: '20px 0', width: '20px'}} />
-          <Comments post={post}/>
-          <Divider />
-          </div>
-
-        </div>
-        {/* <div className={classes.imageSection}>
-          <img className={classes.media} src={post.selectedFile || 'https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg'} alt={post.title} /> 
-        </div> */}
-      </div>
-      <div className={classes.recommendedPosts}>
-    {recommendedPosts.length  && (
-      <div className={classes.section}>
-        <Typography gutterBottom variant="h5">You might also like: </Typography>
-        <Divider />
-        <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 1, sm: 8, md: 12 }}>
-
-        {recommendedPosts.map(({ title, likes, selectedFile, _id}) => (
-          <Grid xs={2} sm={4} md={4}>
-          <div style={{ margin: '20px', cursor: 'pointer'}} onClick={() => openPost(_id)} key={_id}> 
-           <Typography gutterBottom variant="h6">{title}</Typography>
-           <Typography gutterBottom variant="subtitle1">Likes: {likes.length}</Typography>
-           {selectedFile && selectedFile.map((file, index) => (
-       <img key={index} src={file.url} style={{ borderRadius: '15px'}}alt={post.title} width={300} height={300}/>
-        ))}
-          </div>
-          </Grid>
-        ))}
-      
-        </Grid>
-      </div>
-    
-    )}
-  </div>
-    </Paper>
-
-
-
-  )
-}
-
-export default PostDetails
+export default PostDetails;
